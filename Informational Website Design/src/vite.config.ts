5858d3bb316cd59e92b35d73f8187fa002686e2a
InvisiBILL-Design <lisa@getinvisibill.com>
2025-09-14 14:53:49 -0700

    Update vite.config.ts

--- Informational Website Design/src/vite.config.ts
+++ Informational Website Design/src/vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
-      '@': new URL('./src', import.meta.url).pathname,
+      '@': path.resolve(__dirname, '.'),
     },
   },
 })
