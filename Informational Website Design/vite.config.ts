2250fedddf10176a18188a907fd1416584d67d9e
InvisiBILL-Design <lisa@getinvisibill.com>
2025-09-14 15:21:13 -0700

    Update vite.config.ts

--- Informational Website Design/vite.config.ts
+++ Informational Website Design/vite.config.ts
@@ -1,11 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': new URL('./src', import.meta.url).pathname,
-    },
-  },
 })
